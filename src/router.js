decb191f355f02a0bffbfcf7e7be4aae10193f40
Rebecca <224315@buas.nl>
2023-03-11 13:22:32 +0100

    changed routes

--- src/router.js
+++ src/router.js
@@ -4,7 +4,9 @@ import Meta from 'vue-meta'
 
 import Home from './views/home'
 import Blog from './views/blog'
+import Terminal from "@/views/terminal";
 import './style.css'
+import animation from "@/components/organisms/about-me";
 
 Vue.use(Router)
 Vue.use(Meta)
@@ -20,6 +22,11 @@ export default new Router({
       name: 'Blog',
       path: '/blog',
       component: Blog,
+    },
+    {
+      name: 'Terminal',
+      path: '/terminal',
+      component: Terminal,
     },
   ],
 })
