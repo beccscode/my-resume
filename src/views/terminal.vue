3fcc4c52f9414c0e4c40c8b332771a2f695ca753
Rebecca <224315@buas.nl>
2023-03-11 13:21:41 +0100

    about me page updated

---
+++ src/views/terminal.vue
@@ -0,0 +1,20 @@
+<template>
+  <about-me></about-me>
+</template>
+
+<script>
+import aboutMe from "@/components/organisms/about-me";
+
+export default {
+  name: "terminal.vue",
+  components: {
+    aboutMe
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
